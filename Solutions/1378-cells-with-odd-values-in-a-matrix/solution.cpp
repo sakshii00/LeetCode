c03fb5df7183a82cf177cbc9dbfc09593b3cfad8
Sakshi <92578876+sakshii00@users.noreply.github.com>
2023-06-06 13:01:58 +0000

    [LeetCode ] Runtime - 4 ms (42.18%), Memory - 7.8 MB (100.00%)

--- Solutions/1378-cells-with-odd-values-in-a-matrix/solution.cpp
+++ Solutions/1378-cells-with-odd-values-in-a-matrix/solution.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
-vector<vector<int>> updaterow(vector<vector<int>> &arr,vector<int> index,int col,int row){
-        for(int k=0;k<col;k++){
-            arr[index[0]][k]++;
-        }
-
-        for(int k=0;k<row;k++){
-            arr[k][index[1]]++;
-        }
-        return arr;
-    }
     int oddCells(int m, int n, vector<vector<int>>& indices) {
-
-    
-        int s=0;
-        vector<vector<int>> arr(m, vector<int> (n, 0));
+        vector<int> r(m,0);
+        vector<int> c(n,0);
         for(auto i:indices){
-            updaterow(arr,i,n,m);
+            r[i[0]]++;
+            c[i[1]]++;
         }
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if( arr[i][j] %2!=0){
-                    s++;
+        int ans=0;
+        for(int i:r){
+            for(int j:c){
+                if((i+j)%2!=0){
+                    ans++;
                 }
             }
         }
-        return s;
-
+        return ans;
+        
     }
-    
 };
