07118b59f45f1c79aa82b7a082d643ddcfc6be24
Sakshi <92578876+sakshii00@users.noreply.github.com>
2023-09-01 13:57:42 +0000

    [LeetCode ] Runtime - 283 ms (5.07%), Memory - 10.3 MB (99.99%)

--- Solutions/0001-two-sum/solution.cpp
+++ Solutions/0001-two-sum/solution.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> v;
         for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums.size();j++){
-                if((nums[j]+nums[i])==target && i!=j){
-                    v.push_back(i);
-                    v.push_back(j);
-                    break;
-
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    return {i,j};
                 }
             }
-            if(!v.empty()){
-                break;
-            }
         }
-        return v;
+        return {0};
     }
 };
