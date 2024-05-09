6fa5eded196213549970f129948e4594ad1bfe62
Sakshi <92578876+sakshii00@users.noreply.github.com>
2024-05-09 13:04:44 +0000

    [LeetCode ] Runtime - 0 ms (100.00%), Memory - 8.2 MB (13.36%)

---
+++ Solutions/0054-spiral-matrix/solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> v;
+        int t=0,l=0,r=matrix[0].size()-1,b=matrix.size()-1;
+
+        while(t<=b && l<=r){
+
+        for(int i=l;i<=r;i++){
+            v.push_back(matrix[t][i]);
+        }
+        t++;
+
+        for(int i=t;i<=b;i++){
+            v.push_back(matrix[i][r]);
+        }
+        r--;
+        if(t<=b){
+        for(int i=r;i>=l;i--){
+            v.push_back(matrix[b][i]);
+        }
+        b--;
+        }
+
+
+        if(l<=r){
+        for(int i=b;i>=t;i--){
+            v.push_back(matrix[i][l]);
+        }
+        l++;
+        }
+        }
+        return v;
+    }
+};
