cb674184ecefbae2980c85b5e1a45c0ab4268914
Sakshi <92578876+sakshii00@users.noreply.github.com>
2024-05-09 09:15:58 +0000

    [LeetCode ] Runtime - 3 ms (98.96%), Memory - 16.4 MB (91.27%)

--- Solutions/0073-set-matrix-zeroes/solution.cpp
+++ Solutions/0073-set-matrix-zeroes/solution.cpp
@@ -1,31 +1,40 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        set<int> sr,sc;
-        //counting zeroes
+        int col0=1;
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
-                if (matrix[i][j]==0){
-                    sr.insert(i);
-                    sc.insert(j);
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }
+                    else{
+                        col0=0;
+                    }
+                }
+
+            }
+        }
+
+        for(int i=1;i<matrix.size();i++){
+            for(int j=1;j<matrix[0].size();j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
                 }
             }
         }
 
-        for(int i:sr){
-            for(int j=0;j<matrix[0].size();j++){
-                matrix[i][j]=0;
+        if(!matrix[0][0]){
+            for(int i=0;i<matrix[0].size();i++){
+                matrix[0][i]=0;
             }
         }
 
-        
-            for(int j=0;j<matrix.size();j++){
-                for(int i:sc){
-                matrix[j][i]=0;
-                }
+        if(!col0){
+            for(int i=0;i<matrix.size();i++){
+                matrix[i][0]=0;
             }
-
         }
-        
-    
+    }
 };
