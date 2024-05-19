03a0dc6d9118113f96ee780decdfa2b5e7a4709b
Sakshi <92578876+sakshii00@users.noreply.github.com>
2024-05-19 20:41:07 +0000

    [LeetCode ] Runtime - 237 ms (10.41%), Memory - 145.7 MB (6.29%)

---
+++ Solutions/3307-find-the-maximum-sum-of-node-values/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+        int n = nums.size();
+        vector<vector<long long>> temp(n, vector<long long>(2, -1)); 
+        
+        return calMax(nums, n, k, 0, 1, temp);
+    }
+
+private:
+    long long calMax(vector<int>& nums, int n, int k, int curInd, int isEven, vector<vector<long long>>& temp) {
+        if (curInd == n) { 
+            return isEven == 1 ? 0 : LLONG_MIN;
+        }
+        if (temp[curInd][isEven] != -1) {  
+            return temp[curInd][isEven];
+        }
+
+       
+        long long noXor = nums[curInd] + calMax(nums, n, k, curInd + 1, isEven, temp);  
+        long long withXor = (nums[curInd] ^ k) + calMax(nums, n, k, curInd + 1, !isEven, temp);  
+
+        long long mxPossible = max(noXor, withXor);
+        temp[curInd][isEven] = mxPossible;
+        return mxPossible;
+    }
+};
