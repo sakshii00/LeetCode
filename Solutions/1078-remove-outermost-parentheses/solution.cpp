4f3d3899f812bb9d659eab280d733c5538b28491
Sakshi <92578876+sakshii00@users.noreply.github.com>
2024-05-28 13:19:36 +0000

    [LeetCode ] Runtime - 0 ms (100.00%), Memory - 7.8 MB (78.53%)

---
+++ Solutions/1078-remove-outermost-parentheses/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string res;
+        int open=0;
+        for(char c : s){
+           
+            if(c== '(' && open++>0) res+=c;
+            
+            if(c==')' && open-->1) res+=c;
+            
+        }
+        return res;
+    }
+};
