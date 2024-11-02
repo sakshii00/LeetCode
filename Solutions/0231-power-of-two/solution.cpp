59ddd5dcc01d9f06832c4811bc4e83162f9a8c52
Sakshi <92578876+sakshii00@users.noreply.github.com>
2024-11-02 09:43:04 +0000

    [LeetCode ] Runtime - 0 ms (100.00%), Memory - 7.5 MB (73.24%)

--- Solutions/0231-power-of-two/solution.cpp
+++ Solutions/0231-power-of-two/solution.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int digit=n%10;
-        if(n==1){
-            return true;
-        }
-        if(digit==2 || digit==4 || digit==6 || digit==8){
-            for(int i=1;i<31;i++){
-                if(pow(2,i)==n){
-                    return true;
-                }
-            }
-        }
-        return false;
+        return n > 0 && (n & (n - 1)) == 0;
     }
 };
+
