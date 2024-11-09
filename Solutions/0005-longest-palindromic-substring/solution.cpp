5987f372650f288fded3efc4e8cfc07903a48197
Sakshi <92578876+sakshii00@users.noreply.github.com>
2024-11-09 14:23:43 +0000

    [LeetCode ] Runtime - 15 ms (68.49%), Memory - 23.5 MB (44.70%)

--- Solutions/0005-longest-palindromic-substring/solution.cpp
+++ Solutions/0005-longest-palindromic-substring/solution.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        if(s.size()<=1) return s;
-
-        int maxlength=0,len=0;
-        string ans;
-        for(int i=0;i<s.size();i++){
-            int j=i;
-            int k=i;
-            
-            while(k>=0 && j<s.size() && s[j++]==s[k--]){
-                len=j-k-1;
-                
-                if(len>maxlength){
-                    ans=s.substr(k+1,len);
-                    maxlength=len;
-                }
-            }
-            j=i,k=i+1;
-            while(k>=0 && j<s.size() && s[j++]==s[k--]){
-                len=j-k-1;
-                if(len>maxlength){
-                    ans=s.substr(k+1,len);
-                    maxlength=len;
-                }
-            }
-            }
-            
-        return ans;
-        }
-        
+  
+string Palindrome(int center1, int center2, const string &s) {
+    int left = center1;
+    int right = center2;
     
+    // Expand as long as the characters are equal and within bounds
+    while (left >= 0 && right < s.size() && s[left] == s[right]) {
+        left--;
+        right++;
+    }
+
+    // Return the valid palindrome substring
+    return s.substr(left + 1, right - left - 1);
+}
+
+string longestPalindrome(const string &s) {
+    string ans = "";
+    
+    // Iterate through each character to consider both even and odd length palindromes
+    for (int i = 0; i < s.size(); i++) {
+        // Check for even-length palindrome
+        string even = Palindrome(i, i + 1, s);
+
+        // Check for odd-length palindrome
+        string odd = Palindrome(i, i, s);
+
+        // Determine the longer palindrome between even and odd cases
+        string add = (odd.size() > even.size()) ? odd : even;
+        
+        // Update the answer if we find a longer palindrome
+        if (add.size() > ans.size()) {
+            ans = add;
+        }
+    }
+    
+    return ans;
+}
+
 };
