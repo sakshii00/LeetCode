06828bd94a2a8d36a2edb3b18a9a8d4a68b8bbe4
Sakshi <92578876+sakshii00@users.noreply.github.com>
2024-11-10 11:29:49 +0000

    [LeetCode ] Runtime - 0 ms (100.00%), Memory - 20.6 MB (90.71%)

--- Solutions/0268-missing-number/solution.cpp
+++ Solutions/0268-missing-number/solution.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int s=0;
-        for(int i=0;i<nums.size();i++){
-            s+=nums[i];
-        }
+        int sum = accumulate(nums.begin(), nums.end(), 0);
         int n=nums.size();
-        n=(n*(n+1))/2;
-        return n-s;
-
+        int SumNNumbers=(n*(n+1))/2;
+        return SumNNumbers-sum;
+        
     }
 };
