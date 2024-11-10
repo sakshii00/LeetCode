bd77d2cf09666de2dafa91c9633b7a4486516622
Sakshi <92578876+sakshii00@users.noreply.github.com>
2024-11-10 21:21:35 +0000

    [LeetCode ] Runtime - 0 ms (100.00%), Memory - 16.6 MB (39.66%)

--- Solutions/0152-maximum-product-subarray/solution.cpp
+++ Solutions/0152-maximum-product-subarray/solution.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int maxproduct=INT_MIN;
-        int p=1,s=1;
-        for(int i=0;i<nums.size();i++){
-            if(p==0) p=1;
-            if(s==0) s=1;
-            p=p*nums[i];
-            s=s*nums[nums.size()-i-1];
-            maxproduct=max(maxproduct,max(p,s));
+        int n=nums.size();
+        int prefix=1;
+        int suffix=1;
+        int maxPr=INT_MIN;
+        for(int i=0;i<n;i++){
+            //when it is zero we reset
+            if(prefix==0){
+                prefix=1;
+            }
+            if(suffix==0){
+                suffix=1;
+            }
+            //we check from front
+            prefix=prefix*nums[i];
+            //we check from back
+            suffix=suffix*nums[n-i-1];
+            maxPr=max(maxPr,max(suffix, prefix));
         }
-        return maxproduct;
+        return maxPr;
     }
 };
