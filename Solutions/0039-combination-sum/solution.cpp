bebfbcb020708960d9f7e15367f5eb045fa37c24
Sakshi <92578876+sakshii00@users.noreply.github.com>
2024-11-11 12:08:27 +0000

    [LeetCode ] Runtime - 4 ms (44.89%), Memory - 12.6 MB (80.30%)

--- Solutions/0039-combination-sum/solution.cpp
+++ Solutions/0039-combination-sum/solution.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
-    void recursive(int idx, vector<int> &candidates, int target, vector<vector<int>> &ans, vector<int> &temp){
+    void combinationSumHelper(int idx, int target, vector<int> &candidates, vector<int> &temp, vector<vector<int>> &ans){
         if(target==0){
             ans.push_back(temp);
             return;
         }
+        
         if(idx==candidates.size()){
             return;
         }
-        if(candidates[idx]<=target){
+        if(target<candidates[idx]){
+            return;
+        }
+        
+        //pick 
         temp.push_back(candidates[idx]);
-        recursive(idx, candidates, target-candidates[idx], ans, temp);
-        temp.pop_back();}
-        recursive(idx+1, candidates, target, ans, temp);
+        combinationSumHelper(idx, target-candidates[idx], candidates, temp, ans);
+        //not pick
+        temp.pop_back();
+        combinationSumHelper(idx+1, target, candidates, temp, ans);
+        
     }
+    
+    
+    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> temp;
+        //one element can be taken as many times as we like
+        //pick not pick recursion
+        sort(candidates.begin(), candidates.end());
         vector<vector<int>> ans;
-        recursive(0, candidates, target, ans, temp);
+        vector<int> temp;
+        combinationSumHelper(0, target, candidates, temp, ans);
+        
         return ans;
+        
+        
     }
+    
+    
+    
 };
